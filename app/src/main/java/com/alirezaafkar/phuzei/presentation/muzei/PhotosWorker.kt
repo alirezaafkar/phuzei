dffa2e51471573580db751868f2411d947451133
alirezaafkar <pesiran@gmail.com>
2018-12-06 00:39:12 +0330

    Catch photos network call exception

--- app/src/main/java/com/alirezaafkar/phuzei/presentation/muzei/PhotosWorker.kt
+++ app/src/main/java/com/alirezaafkar/phuzei/presentation/muzei/PhotosWorker.kt
@@ -2,7 +2,12 @@ package com.alirezaafkar.phuzei.presentation.muzei
 
 import android.content.Context
 import androidx.core.net.toUri
-import androidx.work.*
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.Worker
+import androidx.work.WorkerParameters
 import com.alirezaafkar.phuzei.App
 import com.alirezaafkar.phuzei.data.model.Media
 import com.alirezaafkar.phuzei.data.model.isImage
@@ -11,18 +16,21 @@ import com.alirezaafkar.phuzei.data.pref.AppPreferences
 import com.alirezaafkar.phuzei.data.repository.PhotosRepository
 import com.google.android.apps.muzei.api.provider.Artwork
 import com.google.android.apps.muzei.api.provider.ProviderContract
+import java.io.IOException
 import javax.inject.Inject
 
 /**
  * Created by Alireza Afkar on 16/9/2018AD.
  */
 class PhotosWorker(
-    context: Context,
-    workerParams: WorkerParameters
+        context: Context,
+        workerParams: WorkerParameters
 ) : Worker(context, workerParams) {
 
-    @Inject lateinit var repository: PhotosRepository
-    @Inject lateinit var prefs: AppPreferences
+    @Inject
+    lateinit var repository: PhotosRepository
+    @Inject
+    lateinit var prefs: AppPreferences
 
     init {
         App.get(context).component?.inject(this)
@@ -30,14 +38,21 @@ class PhotosWorker(
 
     override fun doWork(): Result {
         val album = prefs.album ?: return Result.FAILURE
-        repository.getAlbumPhotosSync(album, prefs.pageToken)?.let {
+
+        val response = try {
+            repository.getAlbumPhotosSync(album, prefs.pageToken)
+        } catch (e: IOException) {
+            null
+        }
+
+        response?.let {
             prefs.pageToken = it.nextPageToken
             onPhotosResult(
-                if (prefs.shuffle) {
-                    it.mediaItems.shuffled()
-                } else {
-                    it.mediaItems
-                }
+                    if (prefs.shuffle) {
+                        it.mediaItems.shuffled()
+                    } else {
+                        it.mediaItems
+                    }
             )
             return Result.SUCCESS
         } ?: kotlin.run {
@@ -49,33 +64,33 @@ class PhotosWorker(
     private fun onPhotosResult(medias: List<Media>) {
         deleteAllImages()
         medias
-            .asSequence()
-            .filter(Media::isImage)
-            .map { photo ->
-                Artwork().apply {
-                    token = photo.id
-                    title = photo.filename
-                    byline = photo.description
-                    webUri = photo.productUrl.toUri()
-                    persistentUri = photo.largeUrl().toUri()
+                .asSequence()
+                .filter(Media::isImage)
+                .map { photo ->
+                    Artwork().apply {
+                        token = photo.id
+                        title = photo.filename
+                        byline = photo.description
+                        webUri = photo.productUrl.toUri()
+                        persistentUri = photo.largeUrl().toUri()
 
+                    }
                 }
-            }
-            .toList().forEach { artwork ->
-                ProviderContract.Artwork.addArtwork(
-                    applicationContext,
-                    PhotosArtProvider::class.java,
-                    artwork
-                )
-            }
+                .toList().forEach { artwork ->
+                    ProviderContract.Artwork.addArtwork(
+                            applicationContext,
+                            PhotosArtProvider::class.java,
+                            artwork
+                    )
+                }
     }
 
     private fun deleteAllImages() {
         val contentUri = ProviderContract.Artwork.getContentUri(
-            applicationContext, PhotosArtProvider::class.java
+                applicationContext, PhotosArtProvider::class.java
         )
         applicationContext.contentResolver.delete(
-            contentUri, null, null
+                contentUri, null, null
         )
     }
 
@@ -83,13 +98,13 @@ class PhotosWorker(
         internal fun enqueueLoad() {
             val workManager = WorkManager.getInstance()
             workManager.enqueue(
-                OneTimeWorkRequestBuilder<PhotosWorker>()
-                    .setConstraints(
-                        Constraints.Builder()
-                            .setRequiredNetworkType(NetworkType.CONNECTED)
+                    OneTimeWorkRequestBuilder<PhotosWorker>()
+                            .setConstraints(
+                                    Constraints.Builder()
+                                            .setRequiredNetworkType(NetworkType.CONNECTED)
+                                            .build()
+                            )
                             .build()
-                    )
-                    .build()
             )
         }
     }
